fcc60421eebe75b0811caef3691500da1467437a
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2024-03-24 14:04:54 +0100

    connect to wallet and display account address

---
+++ crypto-app/src/app/components/index.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  interface Window {
+    __selected: string;
+    __imageSelected: string;
+    __selected2: string;
+    __imageSelected2: string;
+    __button: string;
+    __price1: number;
+    __price2: number;
+  }
+}

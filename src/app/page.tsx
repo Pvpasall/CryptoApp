c889730f79bf14fd97158d5fc571aaf6a57c5acd
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2024-03-24 14:14:29 +0100

    fix folder showing when push to the repo

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useDisclosure } from "@chakra-ui/react";
+import Header from "./components/Header";
+import ConnectButton from "./components/ConnectButton";
+import AccountModal from "./components/modal/AccountModal";
+export default function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Header>
+        <ConnectButton handleOpenModal={onOpen} />
+        <AccountModal isOpen={isOpen} onClose={onClose} />
+      </Header>
+     
+    </>
+  );
+}
